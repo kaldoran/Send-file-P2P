43776075489ded8f325dde771c2fe79b3ecae740
kaldoran <kaldoran@live.fr>
2015-01-27 14:45:22 +0100

    delete useless files

--- run.sh
+++ run.sh
@@ -2,8 +2,17 @@
 # NICOLAS R
 # 27 Janvier 2015
 
-(cd ./Client && make);
-(cd ./Server && make);
+echo -e "\nClient : \n";
+(cd ./Client && make $1);
 
-./Server/BIN/Server
-./Client/BIN/Client
+echo -e "\nServer : \n";
+(cd ./Server && make $1);
+
+if ( [ $1 != "clean" ] ); then 
+	if ( [ -e "./Server/BIN/Server" ] && [ -e "./Client/BIN/Client" ] ); then
+		./Server/BIN/Server
+		./Client/BIN/Client
+	else 
+		echo "Can't launch the Bittorrent program, please re-run 'run.sh' or 'run.sh all'";
+	fi;
+fi;
