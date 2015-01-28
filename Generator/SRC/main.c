a72424962015b0ac6d48d84957614352edb13ae3
kaldoran <kaldoran@live.fr>
2015-01-28 17:38:01 +0100

    Ajout du generateur

---
+++ Generator/SRC/main.c
@@ -0,0 +1,20 @@
+//----------------------------------------------------------
+// AUTEUR : REYNAUD Nicolas                                 |
+// FICHIER : main.c                                         |
+// DATE : 27/01/15                                          |
+//----------------------------------------------------------
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> // For fork and kill
+
+
+int main(int argc, char const *argv[]) {
+    (void)(argc);
+    (void)(argv);
+
+    printf("[[INFO] Server] Welcome to this awesome new project\n");
+    
+    exit(EXIT_SUCCESS);
+}
