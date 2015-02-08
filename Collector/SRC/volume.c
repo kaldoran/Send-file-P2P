50ae79868608c8a8ce631f3a14399eaef11d9a40
Kaldoran <kaldoran@live.fr>
2015-02-08 19:46:44 +0100

    Add function for send volume

--- Collector/SRC/volume.c
+++ Collector/SRC/volume.c
@@ -5,20 +5,27 @@
 //----------------------------------------------------------
 
 #include "volume.h"
+#include "tcp.h"
 
 void getVolume(int volNum, int volSize, Collector* collectors, FILE* file){
     (void) collectors; 
     char read[volSize];
 
     int collector; /* Collector with searched volume's socket id */
-    recv(collector, read, volSize, 0);
+    
+    tcp_action(collector, read, volSize, RECEIVED);
     
     fseek(file, (volSize*volNum-1), SEEK_SET);
         
     fprintf(file, "%s", read);
 }
 
-void sendVolume(Socket *s, int volNum) {
-    (void) s;
-    (void) volNum;
+void sendVolume(Socket *s, int volNum, int volSize, FILE* file) {
+    
+    char buf[volSize];
+    
+    fseek(file, (volSize*volNum-1), SEEK_SET);
+    fread ((char*)buf, volSize, 1, file);
+    
+    tcp_action(index->sock, read, volSize, SEND);
 }
