8c8a69db8e77ba2a036682588901465b73916acb
Bascool <bascol.kevin@hotmail.fr>
2015-02-09 17:12:38 +0100

    collectors volume list ! IN PROGRESS !

--- Collector/SRC/collectors.c
+++ Collector/SRC/collectors.c
@@ -6,20 +6,17 @@
 
 
 #include "collectors.h"
+#include "tcp.h"
 
 
-Collector* new_collect(int nb_vol, char* ip_coll){
+Collector* new_collect(int nb_vol){
     Collector* collect;
     
     if ((collect = calloc(1, sizeof *collect)) == NULL) {
         QUIT_MSG("Can't Allocate Collector");
     }
     
-    collect->id_socket = socket(AF_INET,SOCK_STREAM,0); 
-    
-    if (index->id_socket == -1) {
-        QUIT_MSG("Can't create the socket");
-    }
+    collect->sock = new_socket();
     
     if ((collect->volumes = calloc(nb_vol, sizeof char)) == NULL) {
         QUIT_MSG("Can't Allocate Collector's volume list");
@@ -30,6 +27,38 @@ Collector* new_collect(int nb_vol, char* ip_coll){
     return collect;
 }
 
-void ask_vol_list(Collector* collect){
+void free_collect(Collector *coll) {
+    free_socket(coll->sock);
+    free(coll->volumes);
+    free(index);
+}
+
+Collector** CollectorsFromIps(int nb_coll, char** ips){
+    int i;
+    Collector* coll_list[nb_coll];
+    struct hostent *h;
+
+    for (i = 0; i < nb_coll; ++i){
+        if ((coll_list[i] = calloc(1, sizeof *coll_list[i])) == NULL) {
+            QUIT_MSG("Can't Allocate Collector n°%d", i);
+        }
+    }
     
+    for (i = 0; i < nb_coll; ++i){
+        if( (h = gethostbyname(ips[i])) == NULL ) {
+            QUIT_MSG("Can't Connect to Collector n°%d", i);
+        }
+        else {
+            memcpy(&coll_list[i]->sock->ip, h->h_addr, h->h_length);
+        }
+        (coll_list[i]->sock)->port = COLLECT_PORT;
+        tcp_start(coll_list[i]->sock);
+        ask_vol_list(coll_list)
+    }
+}
+
+void ask_vol_list(Collector* collect){
+    char data[50];
+    
+    tcp_action(collect->sock, "ListOfVolumes", 13, SEND);
 }
