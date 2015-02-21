40c977f9edd1889a48b704b400aa0c481eb23a6b
Nicolas <kaldoran@users.noreply.github.com>
2015-02-22 00:51:37 +0100

    Commentary ping.h

--- Boss/INC/ping.h
+++ Boss/INC/ping.h
@@ -14,10 +14,21 @@
 /* Time in second between 2 presence check */
 #define PING_TIMER 300
 
+/** handle if we need to ask presence or check the respond
+ *  %param block_group : block gorup where you need to handle presence
+ *  %return ! number of second to wait
+ */
 int handlerPresence(blockGroup *block_group);
 
+/** Send a message to all client to check there presence
+ *  %param group : group where you need to ask presence 
+ */
 void askPresence(Group *group);
 
+/** Check if all client had respond
+ *  %param group : group to check
+ *  %param max_socket : pointeur on the max_socket number
+ */ 
 void checkPresence(Group *group, int *max_socket);
 
 #endif /* PING_H included */
