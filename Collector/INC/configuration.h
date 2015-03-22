20bb261f0b697b8b39db0bdbb327f9a83c96563f
Kaldoran <kaldoran@live.fr>
2015-03-22 14:48:05 +0100

    Modification of configuration for adding more information / exemple

--- Collector/INC/configuration.h
+++ Collector/INC/configuration.h
@@ -2,9 +2,10 @@
  * Hi dear user,
  * If you are here it's for configuration i suppose.
  * So if you want to configure you sharing path, juste copy and paste the folloing line
- * #define SHARING_PATH "youpath"
+ * #define SHARING_PATH "yourpath"
  * Copy starting from # and ending at the second ".
  * 
+ * Then replace "yourpath" by an ABSOLUTE path !
  * If you want to remove you setting, you could comment your line by adding "//" at start of it
  *
  * /!\ After changing that file you need ton recompile with the commande "make rebuild"
@@ -17,8 +18,25 @@
  * See exemple at end of file, you could use it by removing "//" 
  */ 
  
-/* These 2 line got the same signification */
+/* Exemple 1 - Linux */
 
+// #define SHARING_PATH "/home/kaldoran/sharing"
 
-// #define SHARING_PATH "./sharing"
+/* Exemple 2 - Windows */
+
+/**
+ * This one will download / use your desktop as sharing path
+ * Be carefull to replace "MyName" by your windows username 
+ */
+// #define SHARING_PATH "C:\\Users\MyName\Desktop"
+
+/* Exemple 3 - Relative */
+
+/**
+ * Relative path could be use, but if you use a relative path, 
+ * This will apply from where you start you programe
+ * These 2 got the same path :
+ */
+
 // #define SHARING_PATH "sharing"
+// #define SHARING_PATH "./sharing"
